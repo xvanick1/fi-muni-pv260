f98356700b89b2e594344c4894454a0acf8d810d
Jozef Vanický <jozef.vanicky@gmail.com>
2021-03-18 16:07:58 +0100

    assignment01 somehow works

--- assignment01/src/main/java/cz/muni/fi/pv260/a01/OperationsCSV.java
+++ assignment01/src/main/java/cz/muni/fi/pv260/a01/OperationsCSV.java
@@ -3,13 +3,16 @@ package cz.muni.fi.pv260.a01;
 import java.util.List;
 
 public class OperationsCSV {
-    public static List<String[]> FilterCSV(List<String[]> csvFile, List<String> methods){
+    public static List<String[]> FilterCSV(List<String[]> csvFile, List<String> methods) throws Exception {
         List<String[]> filteredCSV = null;
         if (methods.contains("filtering")){
             filteredCSV = removeNulls(csvFile);
         }
         if (methods.contains("analytical")){
 
+        }
+        else{
+            throw new Exception("Error: Unknown method, or method input syntax."); // proč to krici?
         }
 
         return filteredCSV;
@@ -20,9 +23,7 @@ public class OperationsCSV {
         for (int i = 0; i<csvFile.size(); i++){
             line = csvFile.get(i);
             for (int j = 0; j < line.length; j++){
-                System.out.print(":" + line[j] + ": ");
                 if ((line[j]).isBlank() || (line[j]).isEmpty() || line[j] == null){
-                    System.out.println("Here, deleting " + i);
                     csvFile.remove(i);
                     i--;
                     break;
