54556acfe04df6eec1f9b5f8aa6ccbe4cf640da9
xdaile <mychalze@gmail.com>
2021-04-01 19:40:01 +0200

    Added two classes, for operator and for number

--- lectureTasks/task03/code_practice-base/src/practicalrefactorings/equationsolver/OperatorNode.java
+++ lectureTasks/task03/code_practice-base/src/practicalrefactorings/equationsolver/OperatorNode.java
@@ -1,4 +1,46 @@
 package practicalrefactorings.equationsolver;
 
-public class OperatorNode {
+public class OperatorNode  implements Evaluable{
+
+    private char operator;
+    private Node left;
+    private Node right;
+
+    /** Construc operator node */
+    public OperatorNode(char symbol) {
+        this.operator = symbol;
+    }
+
+    public boolean hasLeft() {
+        return left != null;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+    @Override
+    public int evaluate() {
+        switch (operator) {
+            case '+':
+                return left.evaluate() + right.evaluate();
+            case '-':
+                return left.evaluate() - right.evaluate();
+            case '*':
+                return left.evaluate() * right.evaluate();
+            case '/':
+                return left.evaluate() / right.evaluate();
+            default:
+                throw new IllegalStateException("Unknown operator: " + operator);
+        }
+    }
+
+    @Override
+    public String representation() {
+
+         return "(" + left.representation() + " " + operator + " " + right.representation() + ")";
+    }
 }
