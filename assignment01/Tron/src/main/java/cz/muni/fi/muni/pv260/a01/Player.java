532f5ec269949790c1d5d031aaa86f0a1e4a1ba0
Jozef Vanický <506486@mail.muni.cz>
2021-04-18 22:19:48 +0200

    Player class - getters, setter generated.

--- assignment01/Tron/src/main/java/cz/muni/fi/muni/pv260/a01/Player.java
+++ assignment01/Tron/src/main/java/cz/muni/fi/muni/pv260/a01/Player.java
@@ -7,5 +7,31 @@ package cz.muni.fi.muni.pv260.a01;
  * @project assignment01
  **/
 public class Player {
+    private int xPosition;
+    private int yPosition;
+    private int currentDirection;
 
+    public int getxPosition() {
+        return xPosition;
+    }
+
+    public void setxPosition(int xPosition) {
+        this.xPosition = xPosition;
+    }
+
+    public int getyPosition() {
+        return yPosition;
+    }
+
+    public void setyPosition(int yPosition) {
+        this.yPosition = yPosition;
+    }
+
+    public int getCurrentDirection() {
+        return currentDirection;
+    }
+
+    public void setCurrentDirection(int currentDirection) {
+        this.currentDirection = currentDirection;
+    }
 }
