5ff0b18fa2e0ec08fc5f2ec8310896550196c49e
xdaile <mychalze@gmail.com>
2021-04-26 22:30:42 +0200

    Create interface for creating games

--- assignment01/Tron/src/main/java/cz/muni/fi/muni/pv260/a01/Game.java
+++ assignment01/Tron/src/main/java/cz/muni/fi/muni/pv260/a01/Game.java
@@ -1,70 +1,23 @@
 package cz.muni.fi.muni.pv260.a01;
 
-import cz.muni.fi.muni.pv260.a01.Controller.ControllerBuilder;
+import cz.muni.fi.muni.pv260.a01.Controller.InputController;
 import cz.muni.fi.muni.pv260.a01.Controller.InputController;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.Window;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
-import java.util.ArrayList;
+import java.awt.*;
 
-public class Game extends Core {
-	ArrayList<Player> players = new ArrayList<>();
-	int moveAmount = 5;
+public interface Game {
 
-	public void init() {
-		super.init();
+    void init();
 
-		Window w = screenManager.getFullScreenWindow();
+    void updateGame();
 
-		createPlayer(40,40,Direction.UP, Color.GREEN, ControllerBuilder.newWASDController());
-		createPlayer(600,440,Direction.DOWN, Color.RED, ControllerBuilder.newArrowsController());
+    void createPlayer(int xPosition, int yPosition, Direction direction, Color color, InputController inputController);
 
-		for(Player player :players){
-			if(player.getController() instanceof KeyListener){
-				w.addKeyListener( (KeyListener) player.getController());
-			}
-			if(player.getController() instanceof MouseListener){
-				w.addMouseListener( (MouseListener) player.getController());
-			}
-		}
-	}
+    void restoreScreen();
 
-	private void createPlayer(int x, int y, Direction startDirection, Color color, InputController inputController) {
-		players.add(new Player(x,y,startDirection, color, inputController));
-	}
+    Graphics2D getGraphics();
 
-	public static void main(String[] args) {
-		new Game().run();
-	}
+    void updateScreen();
 
-	public void draw(Graphics2D g) {
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, screenManager.getWidth(), screenManager.getHeight());
-
-		for(Player player : players){
-			player.updatePosition(moveAmount, screenManager);
-		}
-		for (Player player : players) {
-			checkForCollisions(player.getCurrentPosition());
-		}
-		for(Player player :players) {
-			player.addCurrentPositionsToPath();
-			player.draw(g);
-		}
-	}
-
-	private void checkForCollisions(Point checkedPoint) {
-		for (Player otherPlayer : players) {
-			if (otherPlayer.getPath().contains(checkedPoint)) {
-				System.out.println("Players collided");
-				System.exit(0);
-			}
-		}
-	}
+    ScreenManager getScreenManager();
 }
