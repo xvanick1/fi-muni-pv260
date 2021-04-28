01d8645b921b64bc2e49f2e6a375cc6e5da64c68
Jozef Vanický <506486@mail.muni.cz>
2021-04-29 00:27:12 +0200

    Introduced SnakePath which extends functionality of PathImpl.
    Ready for future updates of SnakeGame.

---
+++ assignment01/Tron/src/main/java/cz/muni/fi/pv260/a01/Snake/SnakePath.java
@@ -0,0 +1,11 @@
+package cz.muni.fi.pv260.a01.Snake;
+
+import cz.muni.fi.pv260.a01.GameEngine.PathImpl;
+
+/**
+ * @author Jozef Vanický
+ * @created 28.04.2021
+ * @project pv260
+ **/
+public class SnakePath extends PathImpl {
+}
