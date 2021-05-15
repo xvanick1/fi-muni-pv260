5b3f3614de895e3df96809d76325347fbf2cd663
Jozef Vanický <506486@mail.muni.cz>
2021-05-15 15:37:36 +0200

    Initialized maven for task03

---
+++ assignment02/src/task03/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+/**
+ * @author Jozef Vanický, Michal Zelenák
+ * @created 14.05.2021
+ * @project pv260-assignment02-task03
+ **/
+
+public class Main {
+    WebDriver driver = new SafariDriver();
+
+}
