650c66462757559eb0fb70a528b42978767bb9f5
Jozef Vanický <506486@mail.muni.cz>
2021-05-15 16:25:26 +0200

    Search Component - successfull search test

--- assignment02/src/task03/src/main/java/Main.java
+++ assignment02/src/task03/src/main/java/Main.java
@@ -1,5 +1,13 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
+
 /**
  * @author Jozef Vanický, Michal Zelenák
  * @created 14.05.2021
@@ -7,6 +15,28 @@ import org.openqa.selenium.safari.SafariDriver;
  **/
 
 public class Main {
-    WebDriver driver = new SafariDriver();
+    @Test
+    public void PositiveSearch(){
+        WebDriver driver = new SafariDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://www.wikipedia.org");
+
+        WebElement searchBar = driver.findElement(By.id("searchInput"));
+        WebElement searchButton = driver.findElement(By.className("pure-button-primary-progressive"));
+
+        searchBar.sendKeys("Masarykova univerzita");
+        searchButton.click();
+
+        String expectedUrl = "https://sk.wikipedia.org/wiki/Masarykova_univerzita";
+        String actualUrl = driver.getCurrentUrl();
+
+        assertEquals(actualUrl, expectedUrl);
+    }
+
+
+
+
+
 
 }
