7d69c83644ce2cef5c899f06223286a68caf4291
xdaile <mychalze@gmail.com>
2021-05-16 21:10:28 +0200

    Mockito version and work changed

--- assignment02/src/task04/src/test/java/cz/muni/fi/pv260/productfilter/AtLeastNOfFilterTest.java
+++ assignment02/src/task04/src/test/java/cz/muni/fi/pv260/productfilter/AtLeastNOfFilterTest.java
@@ -1,7 +1,13 @@
 package cz.muni.fi.pv260.productfilter;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.*;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -11,21 +17,23 @@ import static org.mockito.Mockito.*;
  * @created 14.05.2021
  * @project pv260-assignment02-task03
  **/
-
 public class AtLeastNOfFilterTest {
 
     @Mock
-    Filter filter1 = mock(Filter.class),
-           filter2 = mock(Filter.class),
-           filter3 = mock(Filter.class),
-           filter4 = mock(Filter.class);
+    Filter<Integer> filter1, filter2, filter3, filter4;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     /**
      * Test that the constructor throws FilterNeverSucceeds exception as documented (if n is higher than number of child filters).
      */
     @Test
     public void filterNeverSucceedsConstructorTest() {
-        assertThrows(FilterNeverSucceeds.class, () -> new AtLeastNOfFilter<Integer>(2, filter1));
+        List<Filter<Integer>> filters = new ArrayList<>(Collections.singletonList(filter1));
+        assertThrows(FilterNeverSucceeds.class, () -> new AtLeastNOfFilter<>(2, filters.get(0)));
     }
 
     /**
@@ -33,9 +41,9 @@ public class AtLeastNOfFilterTest {
      */
     @Test
     public void illegalArgumentExceptionConstructorTest() {
-        assertThrows(IllegalArgumentException.class, () -> new AtLeastNOfFilter<Integer>(0, filter1));
-        assertThrows(IllegalArgumentException.class, () -> new AtLeastNOfFilter<Integer>(-1, filter1));
-        assertThrows(IllegalArgumentException.class, () -> new AtLeastNOfFilter<Integer>(-10, filter1));
+        assertThrows(IllegalArgumentException.class, () -> new AtLeastNOfFilter<>(0, filter1));
+        assertThrows(IllegalArgumentException.class, () -> new AtLeastNOfFilter<>(-1, filter1));
+        assertThrows(IllegalArgumentException.class, () -> new AtLeastNOfFilter<>(-10, filter1));
     }
 
     /**
@@ -43,13 +51,13 @@ public class AtLeastNOfFilterTest {
      */
     @Test
     public void passIfExactlyOnePasses(){
-        when(filter1.passes(Color.BLACK)).thenReturn(false);
-        when(filter2.passes(Color.BLACK)).thenReturn(false);
-        when(filter3.passes(Color.BLACK)).thenReturn(true);
-        when(filter4.passes(Color.BLACK)).thenReturn(false);
+        when(filter1.passes(1)).thenReturn(false);
+        when(filter2.passes(1)).thenReturn(false);
+        when(filter3.passes(1)).thenReturn(true);
+        when(filter4.passes(1)).thenReturn(false);
 
-        AtLeastNOfFilter atLeastNOfFilter = new AtLeastNOfFilter(1, filter1, filter2, filter3, filter4);
-        assertTrue(atLeastNOfFilter.passes(Color.BLACK));
+        AtLeastNOfFilter<Integer> atLeastNOfFilter = new AtLeastNOfFilter<>(1, filter1, filter2, filter3, filter4);
+        assertTrue(atLeastNOfFilter.passes(1));
     }
 
     /**
@@ -57,12 +65,12 @@ public class AtLeastNOfFilterTest {
      */
     @Test
     public void FailsIfnMinusOnePasses(){
-        when(filter1.passes(Color.BLACK)).thenReturn(true);
-        when(filter2.passes(Color.BLACK)).thenReturn(true);
-        when(filter3.passes(Color.BLACK)).thenReturn(true);
-        when(filter4.passes(Color.BLACK)).thenReturn(false);
+        when(filter1.passes(1)).thenReturn(true);
+        when(filter2.passes(1)).thenReturn(true);
+        when(filter3.passes(1)).thenReturn(true);
+        when(filter4.passes(1)).thenReturn(false);
 
-        AtLeastNOfFilter atLeastNOfFilter = new AtLeastNOfFilter(4, filter1, filter2, filter3, filter4);
-        assertFalse(atLeastNOfFilter.passes(Color.BLACK));
+        AtLeastNOfFilter<Integer> atLeastNOfFilter = new AtLeastNOfFilter<>(4, filter1, filter2, filter3, filter4);
+        assertFalse(atLeastNOfFilter.passes(1));
     }
 }
