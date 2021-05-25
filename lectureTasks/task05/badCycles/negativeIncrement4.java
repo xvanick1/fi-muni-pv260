7c6c2d3025b3c2043451e181e2ba398a845fa771
xdaile <mychalze@gmail.com>
2021-05-25 02:31:46 +0200

    task1 prepared, cycles created, Check created also

---
+++ lectureTasks/task05/badCycles/negativeIncrement4.java
@@ -0,0 +1,14 @@
+package badCycles;
+
+public class negativeIncrement4 {
+
+    public void negativeIncrement4() {
+
+        for (int i=10; i<0; i-=1) {
+            System.out.println(i);
+        }
+    }
+}
+
+//java -cp checkstyle-VERSION-all.jar com.puppycrawl.tools.checkstyle.gui.Main
+//- pre spustenie gui
